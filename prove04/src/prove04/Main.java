49e88f907ea438b9fca62fe0417b9af84f98a8bc
Conner Andrews <and16054@byui.edu>
2020-01-31 15:33:01 -0700

    Added code to say hello.

--- prove04/src/prove04/Main.java
+++ prove04/src/prove04/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Richard.");
+	    System.out.println("Hello Richard, my name is Conner.");
     }
 }
